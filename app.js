78f7d828b13a65668defccf5b28dfaea47477230
Angel Mauricio <168794154+El-Rincon-Del-Cine@users.noreply.github.com>
2025-02-17 17:00:01 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
-if ('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator && 'PushManager' in window) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('./ws.js')
-            .then((registration) => {
-                console.log('Service Worker registrado', registration.scope);
-                
-                
-                document.getElementById('push').addEventListener('click', () => {
-                    if (Notification.permission === "granted") {
-                        registration.active.postMessage({ type: 'SHOW_NOTIFICATION' });
-                    } else if (Notification.permission !== "denied") {
-                        Notification.requestPermission().then(permission => {
-                            if (permission === "granted") {
-                                registration.active.postMessage({ type: 'SHOW_NOTIFICATION' });
-                            }
-                        });
+        navigator.serviceWorker.register('./sw.js')
+            .then(registration => {
+                console.log('Service Worker registrado con éxito:', registration.scope);
+
+                // Aqui solicita permiso para enviar mi notificacion
+                Notification.requestPermission().then(permission => {
+                    if (permission === 'granted') {
+                        console.log('Notificaciones permitidas.');
+                        registration.showNotification('Gracias por suscribirte a La Taverna del Café');
+                    } else {
+                        console.warn('El usuario denegó las notificaciones.');
                     }
                 });
             })
-            .catch((error) => {
-                console.log('Error al registrar el Service Worker:', error);
+            .catch(error => {
+                console.error('Error al registrar el Service Worker:', error);
             });
     });
 }
