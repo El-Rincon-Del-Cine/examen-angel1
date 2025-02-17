7793d648c29b0e8d3799d00c9ec65b8dbf72d8b4
Angel Mauricio <168794154+El-Rincon-Del-Cine@users.noreply.github.com>
2025-02-17 17:49:42 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('./sw.js') 
+        navigator.serviceWorker.register('./sw.js')
             .then((registration) => {
                 console.log('Service Worker registrado con éxito:', registration.scope);
 
-                // Una vez que el Service Worker esté listo, activamos el botón
-                document.getElementById('notificar').addEventListener('click', async () => {
-                    if (!("Notification" in window)) {
-                        alert("Las notificaciones no son compatibles con tu navegador.");
-                        return;
-                    }
+                navigator.serviceWorker.ready.then((reg) => {
+                    document.getElementById('notificar').addEventListener('click', async () => {
+                        if (!("Notification" in window)) {
+                            alert("Las notificaciones no son compatibles con tu navegador.");
+                            return;
+                        }
 
-                    const permiso = await Notification.requestPermission();
-                    if (permiso === "granted") {
-                        registration.active.postMessage({ type: 'SHOW_NOTIFICATION' });
-                    } else {
-                        alert("No podemos enviarte notificaciones sin tu permiso.");
-                    }
+                        const permiso = await Notification.requestPermission();
+                        if (permiso === "granted") {
+                            reg.active?.postMessage({ type: 'SHOW_NOTIFICATION' });
+                            //enviara una notifiación cada minuto dentro de la app
+                            setInterval(() => {
+                                if (navigator.serviceWorker.controller) {
+                                    navigator.serviceWorker.controller.postMessage({ type: 'SHOW_NOTIFICATION' });
+                                }
+                            }, 60000);
+
+                        } else {
+                            alert("No podemos enviarte notificaciones sin tu permiso.");
+                        }
+                    });
                 });
-
             })
             .catch((error) => {
                 console.error('Error al registrar el Service Worker:', error);
