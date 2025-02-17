b434d050f2f8feb819b55e9747edad693d7d68c1
Angel Mauricio <168794154+El-Rincon-Del-Cine@users.noreply.github.com>
2025-02-17 16:22:38 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('./sw.js')
-            .then((registration) => {
-                console.log('Service Worker iniciado', registration.scope);
-                
-                document.getElementById('notificar').addEventListener('click', () => {
-                    if (Notification.permission === "granted") {
-                        registration.active.postMessage({ type: 'SHOW_NOTIFICATION' });
-                    } else if (Notification.permission !== "denied") {
-                        Notification.requestPermission().then(permission => {
-                            if (permission === "granted") {
-                                registration.active.postMessage({ type: 'SHOW_NOTIFICATION' });
-                            }
-                        });
-                    }
+    navigator.serviceWorker.register('./sw.js')
+        .then(registration => {
+            console.log('Service Worker registrado:', registration);
+            return Notification.requestPermission();
+        })
+        .then(permission => {
+            if (permission === 'granted') {
+                console.log('Permiso de notificaciones concedido');
+
+                // Cuando la página se carga, me dira si deseo permitir notificaciones
+                navigator.serviceWorker.ready.then(registration => {
+                    registration.active.postMessage({ type: 'SHOW_NOTIFICATION' });
                 });
-            })
-            .catch((error) => {
-                console.log('Error al registrar el Service Worker:', error);
-            });
-    });
+            } else {
+                console.warn('Permiso de notificaciones denegado');
+            }
+        })
+        .catch(error => console.error('Error al registrar el Service Worker:', error));
 }
