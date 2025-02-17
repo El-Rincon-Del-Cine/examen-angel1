808055d9034dda9acb4da39b40ec234441aea464
Angel Mauricio <168794154+El-Rincon-Del-Cine@users.noreply.github.com>
2025-02-17 16:09:38 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('./sw.js') 
+        navigator.serviceWorker.register('./sw.js')
             .then((registration) => {
                 console.log('Service Worker registrado con éxito:', registration.scope);
+
+                //esto muestra una ventana para permitir notificaciones
+                if ('Notification' in window) {
+                    Notification.requestPermission().then((result) => {
+                        if (result === 'granted') {
+                            console.log('Permiso de notificaciones concedido');
+                        } else {
+                            console.warn('Permiso de notificaciones denegado');
+                        }
+                    });
+                }
             })
             .catch((error) => {
                 console.error('Error al registrar el Service Worker:', error);
