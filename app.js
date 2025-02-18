784b2bc5a4e4257b701da6df431a1192819dabef
Angel Mauricio <168794154+El-Rincon-Del-Cine@users.noreply.github.com>
2025-02-17 18:35:18 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,11 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('./sw.js')
+        navigator.serviceWorker.register('./sw.js') 
             .then((registration) => {
-                console.log('Service Worker registrado:', registration.scope);
-
-                document.getElementById('notificar').addEventListener('click', async () => {
-                    console.log("Botón presionado. Pidiendo permisos...");
-
-                    const permiso = await Notification.requestPermission();
-                    console.log("Permiso recibido:", permiso);
-
-                    if (permiso === "granted") {
-                        console.log("Permiso concedido. Enviando notificación en 1 minuto...");
-
-                        setTimeout(() => {
-                            if (navigator.serviceWorker.controller) {
-                                console.log("Enviando notificación desde SW...");
-                                navigator.serviceWorker.controller.postMessage({ type: 'SHOW_NOTIFICATION' });
-                            } else {
-                                console.warn("No hay Service Worker activo.");
-                            }
-                        }, 60000); //60000ms son 1 minuto
-
-                    } else {
-                        console.warn("⚠Permiso denegado.");
-                        alert("No podemos enviarte notificaciones sin tu permiso.");
-                    }
-                });
-
+                console.log('Service Worker registrado con éxito:', registration.scope);
             })
             .catch((error) => {
-                console.error('Error al registrar el SW:', error);
+                console.error('Error al registrar el Service Worker:', error);
             });
     });
 }
